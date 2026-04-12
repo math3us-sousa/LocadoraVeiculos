862f2e8e3e5984466c547d795db1df5045549193
Matheus de Sousa <matheussousa@192.168.15.20>
2026-04-12 16:39:05 -0300

    fazendo algumas alterações na tela de login e listar_veiculos

--- sistema/sistema/views.py
+++ sistema/sistema/views.py
@@ -9,15 +9,15 @@ class Login(View):
         return render(request, 'autenticacao.html', contexto)
     
     def post(self, request):
-        email = request.POST.get('email')
-        senha = request.POST.get('senha')
+        nome_usuario = request.POST.get('username')
+        senha = request.POST.get('password')
 
-        usuario = authenticate(request, username=email, password=senha)
+        usuario = authenticate(request, username=nome_usuario, password=senha)
 
         if usuario is not None:
             if usuario.is_active:
                 login(request, usuario)
-                return redirect('home')
+                return redirect('listar_veiculos')
             else:
                 contexto = {'erro': 'Email ou senha inválidos.'}
                 return render(request, 'autenticacao.html', contexto)
